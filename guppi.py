515b39517a2a96af464b4148720bf0eac7ae6c42
Danny Price <dan@thetelegraphic.com>
2016-09-23 15:44:05 -0700

    Modifications for compatibility with Parkes data, added plotting

--- guppi.py
+++ guppi.py
@@ -1,4 +1,4 @@
-""" 
+"""
 # guppi.py
 
 A python file handler for guppi RAW files from the GBT.
@@ -7,22 +7,23 @@ The guppi raw format consists of a FITS-like header, followed by a block of data
 and repeated over and over until the end of the file.
 """
 
-import numpy as np 
+import numpy as np
 import os
 import time
+from pprint import pprint
 
 class EndOfFileError(Exception):
 	pass
 
 class GuppiRaw(object):
-	""" Python class for reading Guppi raw files 
+	""" Python class for reading Guppi raw files
 
 	Args:
 		filename (str): name of the .raw file to open
 
 	Optional args:
 		n_blocks (int): if number of blocks to read is known, set it here.
-		                This saves seeking through the file to check how many 
+		                This saves seeking through the file to check how many
 		                integrations there are in the file.
 	"""
 	def __init__(self, filename, n_blocks=None):
@@ -35,11 +36,11 @@ class GuppiRaw(object):
 		else:
 			self.n_blocks = n_blocks
 
-		self._d = np.zeros(1, dtype='float32')
-    
+		self._d = np.zeros(1, dtype='complex64')
+
  	def __enter__(self):
  		return self
-    
+
  	def __exit__(self, exception_type, exception_value, traceback):
 		self.file_obj.close()
 
@@ -49,11 +50,12 @@ class GuppiRaw(object):
 	def read_header(self):
 		""" Read next header (multiple headers in file)
 
-		Returns: 
+		Returns:
 			(header, data_idx) - a dictionary of keyword:value header data and
 			also the byte index of where the corresponding data block resides.
 		"""
 		start_idx = self.file_obj.tell()
+		key, val = '', ''
 
 		try:
 			header_dict = {}
@@ -77,19 +79,27 @@ class GuppiRaw(object):
 					else:
 						# Otherwise it's an integer
 						val = int(val)
-					
+
 				header_dict[key] = val
 		except ValueError:
+			print line
+			print key, val
 			raise EndOfFileError
 
 		data_idx = self.file_obj.tell()
+
+		#print data_idx % 256
+
+		if data_idx % 256:
+			data_idx += (256 - data_idx % 256)
+
 		self.file_obj.seek(start_idx)
 		return header_dict, data_idx
 
 	def read_first_header(self):
-		""" Read first header in file 
+		""" Read first header in file
 
-		Returns: 
+		Returns:
 			header (dict): keyword:value pairs of header metadata
 		"""
 		self.file_obj.seek(0)
@@ -98,7 +108,7 @@ class GuppiRaw(object):
 		return header_dict
 
 	def read_next_data_block(self):
-		""" Read the next block of data and its header 
+		""" Read the next block of data and its header
 
 		Returns: (header, data)
 			header (dict): dictionary of header metadata
@@ -114,13 +124,13 @@ class GuppiRaw(object):
 		n_samples = header['BLOCSIZE'] / n_chan / n_pol
 
 		d = d.reshape((n_chan, n_samples, n_pol))    # Real, imag
-		
+
 		if self._d.shape != d.shape:
 			self._d = np.zeros(d.shape, dtype='float32')
 
-		self._d = (d.view('complex64'))[:]
+		self._d[:] = d
 
-		return header, self._d
+		return header, self._d[:].view('complex64')
 
 	def find_n_data_blocks(self):
 		""" Seek through the file to find how many data blocks there are in the file
@@ -152,3 +162,39 @@ class GuppiRaw(object):
 	def reset_index(self):
 		""" Return file_obj seek to start of file """
 		self.file_obj.seek(0)
+
+	def print_stats(self):
+		""" Compute some basic stats on the next block of data """
+
+		header, data = self.read_next_data_block()
+		data = data.view('float32')
+
+		print "AVG: %2.3f" % data.mean()
+		print "STD: %2.3f" % data.std()
+		print "MAX: %2.3f" % data.max()
+		print "MIN: %2.3f" % data.min()
+
+		import pylab as plt
+
+	def plot_histogram(self):
+		""" Plot a histogram of data values """
+		import pylab as plt
+		header, data = self.read_next_data_block()
+		data = data.view('float32')
+
+		plt.figure("Histogram")
+		plt.hist(data.flatten(), 33, facecolor='#cc0000')
+		plt.show()
+
+	def plot_spectrum(self):
+		""" Do a (slow) numpy FFT and take power of data """
+		header, data = self.read_next_data_block()
+
+		print "Computing FFT..."
+		d_xx_fft = np.abs(np.fft.fft(data[..., 0]))
+		d_xx_fft = d_xx_fft.flatten()
+
+		print "Plotting..."
+		import pylab as plt
+		plt.plot(10 * np.log10(d_xx_fft))
+		plt.show()
