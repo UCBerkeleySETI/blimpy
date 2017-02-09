71c19aac359cd637726885bdf1f39ecde63a0e59
Danny Price <dan@thetelegraphic.com>
2017-02-08 16:21:42 -0800

    Mods to get fil2hdf entry point running

--- filterbank/__init__.py
+++ filterbank/__init__.py
@@ -3,3 +3,5 @@ from __future__ import absolute_import
 from .filterbank import Filterbank, read_header, fix_header
 from .guppi import GuppiRaw
 from . import utils
+from . import fil2hdf
+from . import gup2hdf
