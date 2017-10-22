7d483d2616c16be54ac9f4909eae8afef04e9f16
Danny Price <dan@thetelegraphic.com>
2017-10-22 20:31:04 +1100

    Change __init__.py for h5py ImportError catch

--- blimpy/__init__.py
+++ blimpy/__init__.py
@@ -3,10 +3,15 @@ from __future__ import absolute_import
 from .filterbank import Filterbank, read_header, fix_header
 from .guppi import GuppiRaw
 from . import utils
-from . import fil2hdf
-from . import fil2h5
-from . import h52fil
-from . import gup2hdf
+
+try:
+    from . import fil2hdf
+    from . import fil2h5
+    from . import h52fil
+    from . import gup2hdf
+except:
+    print("Warning: Cannot import HDF5 utilities")
+    
 from . import waterfall
 from . import file_wrapper
 from . import match_fils
