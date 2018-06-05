dce09e6fdf0e9390b14394581fe8fdda8d5f93cb
Danny Price <dan@thetelegraphic.com>
2018-06-05 14:14:19 +1000

    Putting guppi test as TODO until test data is sourced

--- tests/test_guppi.py
+++ tests/test_guppi.py
@@ -1,27 +1,27 @@
 from blimpy import GuppiRaw
 from pprint import pprint
 
-def test_guppi():
 
-	filename = 'blc1_guppi_57388_HIP113357_0010.0002.raw'
+def todo_test_guppi():
+    filename = 'blc1_guppi_57388_HIP113357_0010.0002.raw'
 
-	guppi = GuppiRaw(filename)
+    guppi = GuppiRaw(filename)
 
-	print guppi
+    print guppi
 
-	header = guppi.read_first_header()
-	
-	pprint(header)
+    header = guppi.read_first_header()
 
-	guppi.read_header()
-	guppi.find_n_data_blocks()
-	header, idx = guppi.read_header()
-	header, data = guppi.read_next_data_block()
-	header, data = guppi.read_next_data_block()
-	header, data = guppi.read_next_data_block()
-	guppi.reset_index()
+    pprint(header)
+
+    guppi.read_header()
+    guppi.find_n_data_blocks()
+    header, idx = guppi.read_header()
+    header, data = guppi.read_next_data_block()
+    header, data = guppi.read_next_data_block()
+    header, data = guppi.read_next_data_block()
+    guppi.reset_index()
 
 
 if __name__ == "__main__":
-	#test_guppi()
-	print("OK")
+    # test_guppi()
+    print("OK")
