3ef8b115c2e70fb9d44db78d75cea9cd6a0bc62f
Danny Price <dan@thetelegraphic.com>
2018-07-09 12:21:50 +1000

    Added max_data_array_size tag in waterfall loader

---
+++ tests/test_heavy.py
@@ -0,0 +1,17 @@
+"""
+# test_voyager_data_load.py
+
+The hard-coded numbers in these tests can be found in the voyager_test_setup.ipynb
+
+"""
+
+import blimpy as bl
+import numpy as np
+import pylab as plt
+
+
+def test_max_data_array_size():
+    fw = bl.Waterfall('Voyager_data/Voyager1.single_coarse.fine_res.fil', max_data_array_size=1)
+
+if __name__ == "__main__":
+    test_max_data_array_size()
