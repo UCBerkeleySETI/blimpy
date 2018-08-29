fe2bbeb73d602ee6591eed3078f1a1529cadb53d
Gijs Molenaar <gijs@pythonic.nl>
2018-08-29 12:12:39 -0700

    don't hardcode voyager data paths
    Former-commit-id: ff6e2dcad44b14fa440916aa6c4cb4d2b5e7244a

--- tests/test_h52fil.py
+++ tests/test_h52fil.py
@@ -2,27 +2,26 @@
 # test_h52fil
 """
 
-import blimpy as bl
-import numpy as np
-import pylab as plt
 import os
+
+import blimpy as bl
+from tests.data import voyager_h5
 
 
 def test_h52fil_conversion():
     ''' Tests the conversion of fil files into h5 in both light and heavy modes.
     '''
 
-    #Creating test file.
-    bl.h52fil.make_fil_file('Voyager_data/Voyager1.single_coarse.fine_res.h5', new_filename = 'test.fil')
+    # Creating test file.
+    bl.h52fil.make_fil_file(voyager_h5, new_filename='test.fil')
 
-    #Creating a "large" test file.
-    bl.h52fil.make_fil_file('Voyager_data/Voyager1.single_coarse.fine_res.h5', new_filename = 'test_large.fil', max_load = 0.001)
+    # Creating a "large" test file.
+    bl.h52fil.make_fil_file(voyager_h5, new_filename='test_large.fil', max_load=0.001)
 
-    #Deleting test file
+    # Deleting test file
     os.remove('test.fil')
     os.remove('test_large.fil')
 
+
 if __name__ == "__main__":
     test_h52fil_conversion()
-
-
