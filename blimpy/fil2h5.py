0eaaa18683bce086e32f9fa1340cce46db0007fa
Emilio Enriquez <jeenriquez@gmail.com>
2018-08-28 17:17:58 -0700

    Adding max_load option to both fil2h5 and h52fil modules.
    Former-commit-id: 2980c22b33bac84e23df6637b7e974ca6d9c4309

--- blimpy/fil2h5.py
+++ blimpy/fil2h5.py
@@ -30,11 +30,11 @@ logging.basicConfig(format=format,stream=stream,level = level_log)
 #------
 
 
-def make_h5_file(filename,out_dir='./', new_filename=None):
+def make_h5_file(filename,out_dir='./', new_filename = None, max_load = None):
     ''' Converts file to HDF5 (.h5) format. Default saves output in current dir.
     '''
 
-    fil_file = Waterfall(filename)
+    fil_file = Waterfall(filename, max_load = max_load)
     if not new_filename:
         new_filename = out_dir+filename.replace('.fil','.h5').split('/')[-1]
     fil_file.write_to_hdf5(new_filename)
@@ -48,6 +48,8 @@ def cmd_tool():
     p.add_option('-o', '--out_dir', dest='out_dir', type='str', default='./', help='Location for output files. Default: local dir. ')
     p.add_option('-n', '--new_filename', dest='new_filename', type='str', default='', help='New name. Default: replaces extention to .h5')
     p.add_option('-d', '--delete_input', dest='delete_input', action='store_true', default=False, help='This option deletes the input file after conversion.')
+    p.add_option('-l', action='store', default=None, dest='max_load', type=float,help='Maximum data limit to load. Default:1GB')
+
     opts, args = p.parse_args(sys.argv[1:])
 
     if len(args)!=1:
@@ -56,7 +58,7 @@ def cmd_tool():
     else:
         filename = args[0]
 
-    make_h5_file(filename, out_dir = opts.out_dir, new_filename=opts.new_filename)
+    make_h5_file(filename, out_dir = opts.out_dir, new_filename = opts.new_filename, max_load = max_load)
 
     if opts.delete_input:
         logger.info("'Deleting input file: %s"%(filename))
