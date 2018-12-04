6b40e1e87b1be92179b0e9cdb7e13fc157516736
Danny Price <dan@thetelegraphic.com>
2018-12-03 22:15:24 -0800

    Moved plotting routines to plotting directory

---
+++ blimpy/plotting/config.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+
+# Check if $DISPLAY is set (for handling plotting on remote machines with no X-forwarding)
+if 'DISPLAY' in os.environ.keys():
+    import pylab as plt
+else:
+    import matplotlib
+    matplotlib.use('Agg')
+    import pylab as plt
+
+from matplotlib.ticker import NullFormatter
+
+plt.rcParams['axes.formatter.useoffset'] = False
+
+
+MAX_PLT_POINTS      = 65536                  # Max number of points in matplotlib plot
+MAX_IMSHOW_POINTS   = (8192, 4096)           # Max number of points in imshow plot
