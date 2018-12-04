cecbffe0def29e0f2be8adc3839a3fecdba1196a
Danny Price <dan@thetelegraphic.com>
2018-12-03 23:05:57 -0800

    Moved ephemeris routines to subpackage
    Former-commit-id: fa869b6b73f1e6f413f6cfc58c326570a7c1e839

---
+++ blimpy/ephemeris/compute_lst.py
@@ -0,0 +1,22 @@
+from .config import *
+
+
+def compute_lst(wf):
+    """ Compute LST for observation """
+    if wf.header[b'telescope_id'] == 6:
+        wf.coords = gbt_coords
+    elif wf.header[b'telescope_id'] == 4:
+        wf.coords = parkes_coords
+    else:
+        raise RuntimeError("Currently only Parkes and GBT supported")
+    if HAS_SLALIB:
+        # dut1 = (0.2 /3600.0) * np.pi/12.0
+        dut1 = 0.0
+        mjd = wf.header[b'tstart']
+        tellong = np.deg2rad(wf.coords[1])
+        last = s.sla_gmst(mjd) - tellong + s.sla_eqeqx(mjd) + dut1
+        # lmst = s.sla_gmst(mjd) - tellong
+        if last < 0.0: last = last + 2.0 * np.pi
+        return last
+    else:
+        raise RuntimeError("This method requires pySLALIB")
