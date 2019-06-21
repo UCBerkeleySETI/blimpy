d1d98716f95e0b86f49d6c19a5d31f4e6acf5d6f
yuhongc <mikechen212@gmail.com>
2019-06-21 12:35:00 -0700

    additional tests for guppi

--- tests/data.py
+++ tests/data.py
@@ -3,3 +3,5 @@ from os import path
 here = path.dirname(path.abspath(__file__))
 voyager_fil = path.join(here, 'Voyager_data/Voyager1.single_coarse.fine_res.fil')
 voyager_h5 = path.join(here, 'Voyager_data/Voyager1.single_coarse.fine_res.h5')
+voyager_raw = path.join(here, 'Voyager_data/blc3_2bit_guppi_57396_VOYAGER1_0006.0013.raw')
+voyager_block1 = path.join(here, 'Voyager_data/Voyager1_block1.npy')
