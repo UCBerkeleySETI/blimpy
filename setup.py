5e45e9a2ef916bf5235fa135bfd348f74dff2778
yuhongc <mikechen212@gmail.com>
2019-06-24 11:47:33 -0700

    add coveralls to requirements

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(name='blimpy',
           'Topic :: Scientific/Engineering :: Astronomy',
       ],
       setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
+      tests_require=['pytest', 'python-coveralls'],
       test_suite="tests",
 )
