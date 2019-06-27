fe0450c6fbe00decc35dddfad0bd4da3c58831ca
yuhongc <mikechen212@gmail.com>
2019-06-27 13:17:04 -0700

    update script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
-python setup.py install; cd tests
-pip install coverage coveralls
+if [[ $DIST == *"py3"* ]]; then
+    python3 setup.py install; cd tests
+    pip3 install coverage coveralls
+else
+    python setup.py install; cd tests
+    pip install coverage coveralls
+fi
 coverage run --source=blimpy -m py.test
 coverage report
 ls -a
-echo $DIST
