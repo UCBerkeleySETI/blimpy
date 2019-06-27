8293f5262914d3de538cabf3f0a772a01f019282
yuhongc <mikechen212@gmail.com>
2019-06-27 14:46:24 -0700

    change package

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 if [[ $DIST == *"py3"* ]]; then
     python3 setup.py install; cd tests
-    pip3 install coverage coveralls pyyaml
+    pip3 install coverage python-coveralls pyyaml
 else
     python setup.py install; cd tests
-    pip install coverage coveralls pyyaml
+    pip install coverage python-coveralls pyyaml
 fi
 apt-get install git
 cd ..
