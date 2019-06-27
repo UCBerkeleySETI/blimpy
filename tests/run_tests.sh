687478ce40eac6b49d4f10d9995f59c87d40bfc4
yuhongc <mikechen212@gmail.com>
2019-06-27 13:30:29 -0700

    update script to install pyyaml

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 if [[ $DIST == *"py3"* ]]; then
     python3 setup.py install; cd tests
-    pip3 install coverage coveralls
+    pip3 install coverage coveralls pyyaml
 else
     python setup.py install; cd tests
-    pip install coverage coveralls
+    pip install coverage coveralls pyyaml
 fi
 cd ..
 coverage run --source=blimpy -m py.test
