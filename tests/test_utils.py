332c35f48c30bbcaff61f7bc15e0f04741b69264
yuhongc <mikechen212@gmail.com>
2019-06-27 18:56:37 -0700

    Revert "add breaking test for travis"
    This reverts commit 2f9e7401e326f6ea4d1b19f0ceb2c71edcb5d4e6 [formerly 9baaebe1a2e673ce71aeeeddc74510323bc89548].
    
    
    Former-commit-id: 3bcb760a3fb6a90ba2d765c76216c200c353bded

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,12 @@
+from blimpy import utils
 import numpy as np
 import pytest
-
-from blimpy import utils
 
 
 def test_utils():
     assert utils.db(100) == 20.0
-    assert utils.lin(20) == 100.0
-    assert utils.closest(np.array([0, 1, 2, 3, 4, 5]), 2.2) == 2
-
+    assert utils.lin(20)  == 100.0
+    assert utils.closest(np.array([0,1,2,3,4,5]), 2.2) == 2
 
 def test_rebin():
     # 1D
@@ -17,11 +15,11 @@ def test_rebin():
     assert np.allclose(aR, np.array([1, 1]))
 
     # 2D
-    b = np.array([[1, 1, 1, 1], [2, 2, 2, 2]])
+    b = np.array([[1,1,1,1], [2,2,2,2]])
     bR = utils.rebin(b, 1, 2)
-    assert np.allclose(bR, [[1, 1], [2, 2]])
+    assert np.allclose(bR, [[1,1], [2,2]])
     bR = utils.rebin(b, None, 2)
-    assert np.allclose(bR, [[1, 1], [2, 2]])
+    assert np.allclose(bR, [[1,1], [2,2]])
     bR = utils.rebin(b, 2, 1)
     assert np.allclose(bR, [1.5, 1.5, 1.5, 1.5])
     bR = utils.rebin(b, 2, None)
@@ -32,12 +30,6 @@ def test_rebin():
         utils.rebin(c, 2, 2)
 
 
-def test_breaking():
-    # make sure travis fails when a test fails
-    assert 1 == 0
-
-
 if __name__ == "__main__":
     test_utils()
     test_rebin()
-    test_breaking()
