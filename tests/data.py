0655827321149976af7d9c1d3d41c4aee0a2cb14
yuhongc <mikechen212@gmail.com>
2019-06-29 13:38:41 -0700

    check if data is downloading

--- tests/data.py
+++ tests/data.py
@@ -4,6 +4,7 @@ import subprocess
 
 subprocess.run(["sh", "download_data.sh"])
 here = path.dirname(path.abspath(__file__))
+print(listdir(here))
 if "Voyager_data" not in listdir(here):
     subprocess.run(["sh", "download_data.sh"])
 voyager_fil = path.join(here, 'Voyager_data/Voyager1.single_coarse.fine_res.fil')
