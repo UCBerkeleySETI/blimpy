e97c4c753cc72885980f980bd8912e215e07fc23
yuhongc <mikechen212@gmail.com>
2019-06-29 13:23:02 -0700

    update data.py

--- tests/data.py
+++ tests/data.py
@@ -1,6 +1,10 @@
-from os import path
+from os import path, listdir
+import subprocess
 
+
+subprocess.run(["sh", "download_data.sh"])
 here = path.dirname(path.abspath(__file__))
+if "Voyager_data" not in listdir(here):
+    subprocess.run(["sh", "download_data.sh"])
 voyager_fil = path.join(here, 'Voyager_data/Voyager1.single_coarse.fine_res.fil')
 voyager_h5 = path.join(here, 'Voyager_data/Voyager1.single_coarse.fine_res.h5')
-print(here)
