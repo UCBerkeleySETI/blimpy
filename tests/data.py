f039f9ab0b0cb2e7fefbbcf1c06bd93b67edcd4d
yuhongc <mikechen212@gmail.com>
2019-06-29 14:22:17 -0700

    updata test init
    Former-commit-id: cc842f2633df95b9d7d953f94b3ac0df279c046d

--- tests/data.py
+++ tests/data.py
@@ -1,14 +1,6 @@
-from os import path, listdir
-import subprocess
-import sys
+from os import path
 
-subprocess.run(["sh", "download_data.sh"])
 here = path.dirname(path.abspath(__file__))
-print(listdir(here))
-if "Voyager_data" not in listdir(here):
-    if sys.version_info >= (3, 0):
-        subprocess.run(["sh", "download_data.sh"])
-    else:
-        subprocess.call(["sh", "download_data.sh"])
+
 voyager_fil = path.join(here, 'Voyager_data/Voyager1.single_coarse.fine_res.fil')
 voyager_h5 = path.join(here, 'Voyager_data/Voyager1.single_coarse.fine_res.h5')
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,8 +8,6 @@ else
     pip install coverage python-coveralls pyyaml
 fi
 apt-get install git
-ls
-pwd
 cd ..
 coverage run --source=blimpy -m pytest
 coverage report
