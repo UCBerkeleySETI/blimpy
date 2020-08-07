0a622166284b52046adc8db8f24e36d6b9be7840
Richard Elkins <richard.elkins@gmail.com>
2020-08-07 06:26:17 -0500

    Just like turbo_seti issue #90
    Travis CI passes even though pytest fails.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -10,5 +10,19 @@ fi
 apt-get install git
 cd ..
 coverage run --source=blimpy -m pytest
+EXITCODE=$?
+if [ $EXITCODE -ne 0 ]; then
+    echo
+    echo '*** Oops, coverage pytest failed, exit code = '$EXITCODE' ***'
+    echo
+    exit $EXITCODE
+fi
 coverage report
+EXITCODE=$?
+if [ $EXITCODE -ne 0 ]; then
+    echo
+    echo '*** Oops, coverage report failed, exit code = '$EXITCODE' ***'
+    echo
+    exit $EXITCODE
+fi
 codecov
