9c26643d3cde83992677bb1259bbabd8c7bcfed0
Danny Price <dancpr@berkeley.edu>
2020-12-08 17:20:34 +1100

    Update plot_waterfall.py

--- blimpy/plotting/plot_waterfall.py
+++ blimpy/plotting/plot_waterfall.py
@@ -15,7 +15,10 @@ def plot_waterfall(wf, f_start=None, f_stop=None, if_id=0, logged=True, cb=True,
     """
 
     plot_f, plot_data = wf.grab_data(f_start, f_stop, if_id)
-
+    
+    # imshow does not support int8, so convert to floating point
+    plot_data = plot_data.astype('float32')
+    
     # Using accending frequency for all plots.
     if wf.header['foff'] < 0:
         plot_data = plot_data[..., ::-1]  # Reverse data
