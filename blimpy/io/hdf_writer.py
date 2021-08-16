d12473b881d8d13bf7da78050be08e4b09e38ab3
Richard Elkins <richard.elkins@gmail.com>
2021-08-16 18:57:27 -0500

    Amend hdf_writer.py to fix issue #232

--- blimpy/io/hdf_writer.py
+++ blimpy/io/hdf_writer.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import time
 import h5py
 import hdf5plugin
@@ -36,8 +38,6 @@ def __write_to_hdf5_heavy(wf, filename_out, f_scrunch=None, *args, **kwargs):
         f_scrunch (int or None): Average (scrunch) N channels together
     """
 
-    block_size = 0
-
     # Note that a chunk is not a blob!!
     # chunk_dim = wf._get_chunk_dimensions() <-- seems intended for raw to fil
     # And, chunk dimensions should not exceed the Waterfall selection shape dimensions.
@@ -49,98 +49,124 @@ def __write_to_hdf5_heavy(wf, filename_out, f_scrunch=None, *args, **kwargs):
     blob_dim  = wf._get_blob_dimensions(chunk_dim)
     n_blobs   = wf.container.calc_n_blobs(blob_dim)
 
-    with h5py.File(filename_out, 'w') as h5:
+    # ===============================
+    # Attempt to write the HDF5 file.
+    # ===============================
+   
+    try:
+        os.remove(filename_out)  # Try to pre-remove output .h5 file.
+    except:
+        pass
+    
+    try:
+        with h5py.File(filename_out, 'w') as h5:
+    
+            h5.attrs['CLASS'] = 'FILTERBANK'
+            h5.attrs['VERSION'] = '1.0'
+    
+            bs_compression = hdf5plugin.Bitshuffle(nelems=0, lz4=True)['compression']
+            bs_compression_opts = hdf5plugin.Bitshuffle(nelems=0, lz4=True)['compression_opts']
+    
+            dout_shape     = list(wf.selection_shape)    # Make sure not a tuple
+            dout_chunk_dim = list(chunk_dim)
+    
+            if f_scrunch is not None:
+                dout_shape[-1] //= f_scrunch
+                dout_chunk_dim[-1] //= f_scrunch
+                wf.header['foff'] *= f_scrunch
+    
+            dset = h5.create_dataset('data',
+                                     shape=tuple(dout_shape),
+                                     chunks=tuple(dout_chunk_dim),
+                                     compression=bs_compression,
+                                     compression_opts=bs_compression_opts,
+                                     dtype=wf.data.dtype)
+    
+            dset_mask = h5.create_dataset('mask',
+                                          shape=tuple(dout_shape),
+                                          chunks=tuple(dout_chunk_dim),
+                                          compression=bs_compression,
+                                          compression_opts=bs_compression_opts,
+                                          dtype='uint8')
+    
+            dset.dims[2].label = b"frequency"
+            dset.dims[1].label = b"feed_id"
+            dset.dims[0].label = b"time"
+    
+            dset_mask.dims[2].label = b"frequency"
+            dset_mask.dims[1].label = b"feed_id"
+            dset_mask.dims[0].label = b"time"
+    
+            # Copy over header information as attributes
+            for key, value in wf.header.items():
+                dset.attrs[key] = value
+    
+            if blob_dim[wf.freq_axis] < wf.selection_shape[wf.freq_axis]:
+    
+                wf.logger.info('Using %i n_blobs to write the data.'% n_blobs)
+                for ii in range(0, n_blobs):
+                    wf.logger.info('Reading %i of %i' % (ii + 1, n_blobs))
+    
+                    bob = wf.container.read_blob(blob_dim, n_blob=ii)
+    
+                    #-----
+                    #Using channels instead of frequency.
+                    c_start = wf.container.chan_start_idx + ii * blob_dim[wf.freq_axis]
+                    t_start = wf.container.t_start + (c_start / wf.selection_shape[wf.freq_axis]) * blob_dim[wf.time_axis]
+                    t_stop = t_start + blob_dim[wf.time_axis]
+    
+                    c_start = (c_start) % wf.selection_shape[wf.freq_axis]
+                    c_stop = c_start + blob_dim[wf.freq_axis]
+                    #-----
+    
+                    if f_scrunch is not None:
+                        c_start //= f_scrunch
+                        c_stop  //= f_scrunch
+                        bob = utils.rebin(bob, n_z=f_scrunch)
+    
+                    wf.logger.debug(t_start,t_stop,c_start,c_stop)
+                    dset[t_start:t_stop,0,c_start:c_stop] = bob[:]
+    
+            else:
+    
+                wf.logger.info('Using %i n_blobs to write the data.'% n_blobs)
+                for ii in range(0, n_blobs):
+                    wf.logger.info('Reading %i of %i' % (ii + 1, n_blobs))
+    
+                    bob = wf.container.read_blob(blob_dim, n_blob=ii)
+                    t_start = wf.container.t_start + ii * blob_dim[wf.time_axis]
+    
+                    #This prevents issues when the last blob is smaller than the others in time
+                    if (ii+1)*blob_dim[wf.time_axis] > wf.n_ints_in_file:
+                        t_stop = wf.n_ints_in_file
+                    else:
+                        t_stop = (ii+1)*blob_dim[wf.time_axis]
+    
+                    if f_scrunch is not None:
+                        bob = utils.rebin(bob, n_z=f_scrunch)
+    
+                    dset[t_start:t_stop] = bob[:]
 
-        h5.attrs['CLASS'] = 'FILTERBANK'
-        h5.attrs['VERSION'] = '1.0'
+        # =================================
+        # Success!  The .h5 file is closed.
+        # =================================
 
-        bs_compression = hdf5plugin.Bitshuffle(nelems=0, lz4=True)['compression']
-        bs_compression_opts = hdf5plugin.Bitshuffle(nelems=0, lz4=True)['compression_opts']
-
-        dout_shape     = list(wf.selection_shape)    # Make sure not a tuple
-        dout_chunk_dim = list(chunk_dim)
-
-        if f_scrunch is not None:
-            dout_shape[-1] //= f_scrunch
-            dout_chunk_dim[-1] //= f_scrunch
-            wf.header['foff'] *= f_scrunch
-
-        dset = h5.create_dataset('data',
-                                 shape=tuple(dout_shape),
-                                 chunks=tuple(dout_chunk_dim),
-                                 compression=bs_compression,
-                                 compression_opts=bs_compression_opts,
-                                 dtype=wf.data.dtype)
-
-        dset_mask = h5.create_dataset('mask',
-                                      shape=tuple(dout_shape),
-                                      chunks=tuple(dout_chunk_dim),
-                                      compression=bs_compression,
-                                      compression_opts=bs_compression_opts,
-                                      dtype='uint8')
-
-        dset.dims[2].label = b"frequency"
-        dset.dims[1].label = b"feed_id"
-        dset.dims[0].label = b"time"
-
-        dset_mask.dims[2].label = b"frequency"
-        dset_mask.dims[1].label = b"feed_id"
-        dset_mask.dims[0].label = b"time"
-
-        # Copy over header information as attributes
-        for key, value in wf.header.items():
-            dset.attrs[key] = value
-
-        if blob_dim[wf.freq_axis] < wf.selection_shape[wf.freq_axis]:
-
-            wf.logger.info('Using %i n_blobs to write the data.'% n_blobs)
-            for ii in range(0, n_blobs):
-                wf.logger.info('Reading %i of %i' % (ii + 1, n_blobs))
-
-                bob = wf.container.read_blob(blob_dim, n_blob=ii)
-
-                #-----
-                #Using channels instead of frequency.
-                c_start = wf.container.chan_start_idx + ii * blob_dim[wf.freq_axis]
-                t_start = wf.container.t_start + (c_start / wf.selection_shape[wf.freq_axis]) * blob_dim[wf.time_axis]
-                t_stop = t_start + blob_dim[wf.time_axis]
-
-                # Reverse array if frequency axis is flipped
-#                     if self.header['foff'] < 0:
-#                         c_stop = self.selection_shape[self.freq_axis] - (c_start)%self.selection_shape[self.freq_axis]
-#                         c_start = c_stop - blob_dim[self.freq_axis]
-#                     else:
-                c_start = (c_start) % wf.selection_shape[wf.freq_axis]
-                c_stop = c_start + blob_dim[wf.freq_axis]
-                #-----
-
-                if f_scrunch is not None:
-                    c_start //= f_scrunch
-                    c_stop  //= f_scrunch
-                    bob = utils.rebin(bob, n_z=f_scrunch)
-
-                wf.logger.debug(t_start,t_stop,c_start,c_stop)
-                dset[t_start:t_stop,0,c_start:c_stop] = bob[:]
-
-        else:
-
-            wf.logger.info('Using %i n_blobs to write the data.'% n_blobs)
-            for ii in range(0, n_blobs):
-                wf.logger.info('Reading %i of %i' % (ii + 1, n_blobs))
-
-                bob = wf.container.read_blob(blob_dim, n_blob=ii)
-                t_start = wf.container.t_start + ii * blob_dim[wf.time_axis]
-
-                #This prevents issues when the last blob is smaller than the others in time
-                if (ii+1)*blob_dim[wf.time_axis] > wf.n_ints_in_file:
-                    t_stop = wf.n_ints_in_file
-                else:
-                    t_stop = (ii+1)*blob_dim[wf.time_axis]
-
-                if f_scrunch is not None:
-                    bob = utils.rebin(bob, n_z=f_scrunch)
-
-                dset[t_start:t_stop] = bob[:]
+    except Exception as ex1: # Something went wrong!
+        wf.logger.error("blimpy __write_to_hdf5_heavy: Writing the output HDF5 file {} failed!"
+                       .format(filename_out))
+        print(repr(ex1))
+        try:
+            if os.path.exists(filename_out):
+                os.remove(filename_out) # scrap a potentially corrupted HDF5 file.
+                # Removal succeeded.  Exit to the O/S with a nonzero exit code.
+                wf.logger.info("blimpy __write_to_hdf5_heavy: Removal of partial HDF5 file {} succeeded."
+                               .format(filename_out))
+            sys.exit(86)
+        except Exception as ex2:
+            wf.logger.error("blimpy __write_to_hdf5_heavy: Removal of partial HDF5 file {} failed!"
+                           .format(filename_out))
+            print(repr(ex2))
+            sys.exit(86)
 
 
 def __write_to_hdf5_light(wf, filename_out, f_scrunch=None, *args, **kwargs):
@@ -151,8 +177,11 @@ def __write_to_hdf5_light(wf, filename_out, f_scrunch=None, *args, **kwargs):
         f_scrunch (int or None): Average (scrunch) N channels together
     """
 
-    block_size = 0
-
+    try:
+        os.remove(filename_out)  # Try to pre-remove output .h5 file.
+    except:
+        pass # Ignore errors.
+    
     with h5py.File(filename_out, 'w') as h5:
 
         h5.attrs['CLASS']   = 'FILTERBANK'
