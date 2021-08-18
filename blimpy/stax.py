9065da3556aecf4ae524fea07dd88f1121931053
Richard Elkins <richard.elkins@gmail.com>
2021-08-18 17:38:34 -0500

    Add files via upload

--- blimpy/stax.py
+++ blimpy/stax.py
@@ -1,197 +1,299 @@
+#!/usr/bin/env python3
+
 r"""
-Make waterfall plots of s set of files.
+Make waterfall plots of a file set, view from top to bottom.
 """
 
 import os
 import sys
-from os.path import isdir
+from os.path import dirname, abspath, isdir
+import gc
 from argparse import ArgumentParser
 import logging
 logger_name = "stax"
 logger = logging.getLogger(logger_name)
 logger.setLevel(logging.INFO)
-from PIL import Image
 
 # Plotting packages import
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use("agg")
 
+# Math/Science package imports
+import numpy as np
+
 # Blimpy imports
-from blimpy import Waterfall
-from blimpy.plotting import plot_waterfall
+import blimpy as bl
+from blimpy.utils import rebin
+
+# Plotting constants
+fontsize=16
+font = {"family" : "DejaVu Sans",
+"size" : fontsize}
+MAX_IMSHOW_POINTS = (4096, 1268)
 
 
-def image_stitch(orientation, png_collection, path_saved_png):
-    r""" Stitch together multiple PNGs into one"""
-
-    logger.info("Stitching together the images, orientation is {}".format(orientation))
-
-    # Empty array to store loaded images.
-    img_collection = []
-    num_pngs = len(png_collection)
-
-    # Loop to load images.
-    ii = 0
-    for png_file in png_collection:
-        img = Image.open(png_file)
-        if ii == 0:
-            base_width, base_height = img.size
-            logger.info("Base (first) image file pixel width = {}, height = {}"
-                        .format(base_width, base_height))
-        width, height = img.size
-        if width != base_width or height != base_height:
-            logger.warning("Skipping {} because pixel width = {}, height = {}, doesn't match the base!"
-                           .format(png_collection[ii], width, height))
-            continue
-        img_collection.append(img)
-        ii += 1
-
-    # Get dimensions of the last image (same as the rest).
-
-    # Creating the template for the stitched image.
-    if orientation == "h":
-        new_img = Image.new("RGBA", (num_pngs * width, height))
-    else:
-        new_img = Image.new("RGBA", (width, num_pngs * height))
-
-    # Initalising the template before pasting the images.
-    init_width = 0
-    init_height = 0
-
-    # Pasting the images in sequence.
-    counter = 0
-    for img in img_collection:
-        box = (init_width, init_height, init_width + width, init_height + height)
-        try:
-            new_img.paste(img, box)
-        except ValueError:
-            logger.error("stax: *** Oops, Image paste error, init_width={}, init_height={}, width={}, height={}"
-                         .format(init_width, init_height, width, height))
-            sys.exit(86)
-        counter += 1
-        if orientation == "h":
-            init_width += width
-        else:
-            init_height += height
-
-    # Save the output, re-sized.
-    if orientation == "h":
-        new_img = new_img.resize((num_pngs * width, height))
-    else:
-        new_img = new_img.resize((width, num_pngs * height))
-    new_img.save(path_saved_png)
-    logger.info("Concatenated {} images to {}".format(counter, path_saved_png))
-
-
-def make_waterfall_plots(file_list, plot_dir, width, height, dpi, f_start, f_stop):
+def plot_waterfall(wf, filename, f_start=None, f_stop=None, **kwargs):
     r"""
-    Make waterfall plots of a given huge-ish file.
-​
+    Plot waterfall of data in a .fil or .h5 file.
+
     Parameters
     ----------
-    file_list : list
-        List of paths of Filterbank or HDF5 input file to plot.
-    plot_dir : str
-        Directory for storing the PNG files.
-    width: float
-        Width of plots in inches.
-    height: float
-        Height of plots in inches.
-    dpi: int
-        Dots per inch.
-    f_start: float
-        Starting frequency or None
-    f_stop: float
-        Stopping frequency or None
+    wf : blimpy.Waterfall object
+        Waterfall object of an H5 or Filterbank file containing the dynamic spectrum data.
+    filename : str
+        Name of the file being plotted.
+    f_start : float
+        Start frequency, in MHz.
+    f_stop : float
+        Stop frequency, in MHz.
+    kwargs : dict
+        Keyword args to be passed to matplotlib imshow().
+
+    Notes
+    -----
+    Plot a single-panel waterfall plot (frequency vs. time vs. intensity)
+    for one of the files in the set of interest, at the
+    frequency of the expected event.
     """
 
-    # Get directory path for storing PNG file
-    if not isdir(plot_dir):
-        os.mkdir(plot_dir)
+    # prepare font
+    matplotlib.rc("font", **font)
 
-    # Begin PNG file collection.
-    png_collection = []
+    # Load in the data from fil
+    plot_f, plot_data = wf.grab_data(f_start=f_start, f_stop=f_stop)
 
-    # Generate a plot/PNG for each frequency chunk.
-    logger.info("Image width = {}, height = {}, dpi = {}"
-                .format(width, height, dpi))
-    count = 0
-    num_files = len(file_list)
-    for input_file in file_list:
+    # Make sure waterfall plot is under 4k*4k
+    dec_fac_x, dec_fac_y = 1, 1
+
+    # rebinning data to plot correctly with fewer points
+    try:
+        if plot_data.shape[0] > MAX_IMSHOW_POINTS[0]:
+            dec_fac_x = plot_data.shape[0] / MAX_IMSHOW_POINTS[0]
+        if plot_data.shape[1] > MAX_IMSHOW_POINTS[1]:
+            dec_fac_y =  int(np.ceil(plot_data.shape[1] /  MAX_IMSHOW_POINTS[1]))
+        plot_data = rebin(plot_data, dec_fac_x, dec_fac_y)
+    except Exception as ex:
+        print("\n*** Oops, grab_data returned plot_data.shape={}, plot_f.shape={}"
+              .format(plot_data.shape, plot_f.shape))
+        print("Waterfall info for {}:".format(wf.filename))
+        wf.info()
+        raise ValueError("*** Something is wrong with the grab_data output!") from ex
+
+    # Rolled back PR #82
+
+    # determine extent of the plotting panel for imshow
+    extent=(plot_f[0], plot_f[-1], (wf.timestamps[-1]-wf.timestamps[0])*24.*60.*60, 0.0)
+
+    # plot and scale intensity (log vs. linear)
+    kwargs["cmap"] = kwargs.get("cmap", "viridis")
+    plot_data = 10.0 * np.log10(plot_data)
+
+    # get normalization parameters
+    vmin = plot_data.min()
+    vmax = plot_data.max()
+    normalized_plot_data = (plot_data - vmin) / (vmax - vmin)
+
+    # display the waterfall plot
+    this_plot = plt.imshow(normalized_plot_data,
+        aspect="auto",
+        rasterized=True,
+        interpolation="nearest",
+        extent=extent,
+        **kwargs
+    )
+
+    # add plot labels
+    plt.xlabel("Frequency [Hz]",fontdict=font)
+    plt.ylabel("Time [s]",fontdict=font)
+
+    # add source name
+    ax = plt.gca()
+    plt.text(0.03, 0.8, filename, transform=ax.transAxes, bbox=dict(facecolor="white"))
+
+    return this_plot
+
+
+def sort2(x, y):
+    r""" Return lowest value, highest value"""
+    if y < x:
+        return y, x
+    return x, y
+
+
+def make_waterfall_plots(file_list, plot_dir, f_start=None, f_stop=None, **kwargs):
+    r"""
+    Make waterfall plots of a file set, view from top to bottom.
+
+    Parameters
+    ----------
+    file_list : str
+        List of filterbank files to plot in a stacked mode.
+    f_start : float
+        Start frequency, in MHz.
+    f_stop : float
+        Stop frequency, in MHz.
+    source_name_list : list
+        List of source names in the set, in order.
+    kwargs : dict
+        Keyword args to be passed to matplotlib imshow().
+    """
+
+    # prepare for plotting
+    matplotlib.rc("font", **font)
+
+    # set up the sub-plots
+    n_plots = len(file_list)
+    fig = plt.subplots(n_plots, sharex=True, sharey=True,figsize=(10, 2*n_plots))
+
+    # get directory path for storing PNG files
+    if plot_dir is None:
+        dirpath = dirname(abspath(file_list[0])) + "/"
+    else:
+        if not isdir(plot_dir):
+            os.mkdir(plot_dir)
+        dirpath = plot_dir
+
+
+    # read in data for the first panel
+    max_load = bl.calcload.calc_max_load(file_list[0])
+    #print("plot_event make_waterfall_plots: max_load={} is required for {}".format(max_load, file_list[0]))
+    wf1 = bl.Waterfall(file_list[0], max_load=max_load)
+    t0 = wf1.header["tstart"]
+    source_name = wf1.header["source_name"]
+
+    # Fix frequency boundaries if required.
+    freqs = wf1.container.populate_freqs()
+    if f_start is None:
+        f_start = freqs[0]
+    if f_stop is None:
+        f_stop = freqs[-1]
+    the_lowest, the_highest = sort2(f_start, f_stop)
+
+    # rebin data to plot correctly with fewer points
+    plot_f1, plot_data1 = wf1.grab_data(f_start=the_lowest, f_stop=the_highest)
+    dec_fac_x, dec_fac_y = 1, 1
+    if plot_data1.shape[0] > MAX_IMSHOW_POINTS[0]:
+        dec_fac_x = plot_data1.shape[0] / MAX_IMSHOW_POINTS[0]
+    if plot_data1.shape[1] > MAX_IMSHOW_POINTS[1]:
+        dec_fac_y =  int(np.ceil(plot_data1.shape[1] /  MAX_IMSHOW_POINTS[1]))
+    plot_data1 = rebin(plot_data1, dec_fac_x, dec_fac_y)
+
+    # Compute the midpoint.
+    the_midpoint = np.abs(the_lowest + the_highest) / 2.
+
+    # Fill in each subplot for the full plot
+    subplots = []
+    for ii, filename in enumerate(file_list):
+        logger.debug("make_waterfall_plots: file {} in list: {}".format(ii, filename))
+        # identify panel
+        subplot = plt.subplot(n_plots, 1, ii + 1)
+        subplots.append(subplot)
 
         # read in data
-        wf = Waterfall(input_file, f_start=f_start, f_stop=f_stop)
+        max_load = bl.calcload.calc_max_load(filename)
+        wf = bl.Waterfall(filename, max_load=max_load)
 
         # Validate frequency range.
-        count += 1
-        logger.info("stax: Processing file {} of {}: {}"
-                    .format(count, num_files, input_file))
+        freqs = wf.container.populate_freqs()
+        ii_lowest, ii_highest = sort2(freqs[0], freqs[-1])
+        logger.info("Processing: {}, freq lowest={}, highest={}".format(filename, ii_lowest, ii_highest))
+        if the_lowest < ii_lowest or the_highest > ii_highest:
+            logger.warning("Frequency range not compatible!  Ignoring this file.")
+            # Protect RAM utilisation.
+            del wf, freqs
+            gc.collect()
+            continue # Skip this file.
 
-        # Make plot for this frequency chunk with plot_waterfall().
+        # make plot with plot_waterfall
         source_name = wf.header["source_name"]
-        plt.figure(source_name, figsize=(width, height), dpi=dpi)
-        plot_waterfall(wf, f_start=f_start, f_stop=f_stop)
+        this_plot = plot_waterfall(wf,
+                                   os.path.basename(filename),
+                                   f_start=the_lowest,
+                                   f_stop=the_highest,
+                                   **kwargs)
 
-        # Save the figures.
-        path_png = plot_dir + "stax_" + str(count) + "_" + source_name + ".png"
-        png_collection.append(path_png)
-        plt.savefig(path_png, dpi=dpi)
-        logger.info("Saved plot: {}".format(path_png))
-        plt.close("all")
+        # Title the full plot if processing the first file.
+        if ii == 0:
+            plot_title = "%s \n MJD:%5.5f (first file)" % (source_name, t0)
+            plt.title(plot_title)
+            max_plot = this_plot
 
-    return png_collection
+        # Format full plot.
+        if ii < len(file_list)-1:
+            plt.xticks(np.linspace(the_lowest, the_highest, num=4), ["","","",""])
+
+    # More overall plot formatting, axis labelling.
+    factor = 1e6
+    units = "Hz"
+    xloc = np.linspace(the_lowest, the_highest, 5)
+    xticks = [round(loc_freq) for loc_freq in (xloc - the_midpoint) * factor]
+    if np.max(xticks) > 1000:
+        xticks = [xt / 1000 for xt in xticks]
+        units = "kHz"
+    plt.xticks(xloc, xticks)
+    plt.xlabel("Relative Frequency [%s] from %f MHz" % (units, the_midpoint), fontdict=font)
+
+    # Add colorbar.
+    cax = fig[0].add_axes([0.94, 0.11, 0.03, 0.77])
+    fig[0].colorbar(max_plot, cax=cax, label="Normalized Power (Arbitrary Units)")
+
+    # Adjust plots
+    plt.subplots_adjust(hspace=0,wspace=0)
+
+    # Save the figures.
+    path_png = dirpath + "stax_fstart_{:0.6f}".format(f_start) \
+                + "_fstop_{:0.6f}".format(f_stop) + ".png"
+    plt.savefig(path_png, bbox_inches="tight")
+    logger.info("Saved plot: {}".format(path_png))
+
+    # show figure before closing if this is an interactive context
+    mplbe = matplotlib.get_backend()
+    logger.debug("make_waterfall_plots: backend = {}".format(mplbe))
+    if mplbe != "agg":
+        plt.show()
+
+    # close all figure windows
+    plt.close("all")
 
 
 def cmd_tool(args=None):
     r"""Coomand line parser"""
-    parser = ArgumentParser(description="Make waterfall plots from a single file.")
+    parser = ArgumentParser(description='Make waterfall plots from a set of files, viewed from top to bottom.')
     parser.add_argument('file_list', type=str, nargs='+', help='List of files to plot')
-    parser.add_argument("--plot_dir", "-p", type=str, default=".", help="Directory to receive plots (.png).  Default: current directory.")
     parser.add_argument('--f_start', type=float, default=None, help='Start frequency.  Default: None.')
     parser.add_argument('--f_stop', type=float, default=None, help='Stop frequency.  Default: None.')
-    parser.add_argument("--stitch", "-s", type=str, default="v", choices=["n", "v", "h"],
-                        help="Stitch files: n(no stitching), v(vertical), or h(horizontal).  Default: v.")
-    parser.add_argument("--dpi", "-d", type=int, default=200,
-                        help="Single image file dots per inch.  Default: 200.")
-    parser.add_argument("--width", "-w", type=float, default=10,
-                        help="Single image file image width in inches.  Default: 10.0.")
-    parser.add_argument("--height", "-t", type=float, default=8,
-                        help="Single image file image height in inches.  Default: 8.0.")
-
+    parser.add_argument('--plot_dir', '-p', type=str, default=".", help='Directory to receive the plot (.png).  Default: current directory.')
     if args is None:
         args = parser.parse_args()
     else:
         args = parser.parse_args(args)
-    if args.dpi < 50:
-        logger.error("stax: --dpi must be at least 50 but I saw: {}!".format(args.dpi))
+    if args.file_list == []:
         os.system("stax -h")
-        sys.exit(86)
-    if args.width < 6:
-        logger.error("stax: --width must be at least 6 but I saw: {}!".format(args.width))
-        os.system("stax -h")
-        sys.exit(86)
-    if args.height < 5:
-        logger.error("stax: --height must be at least 5 but I saw: {}!".format(args.height))
-        os.system("stax -h")
-        sys.exit(86)
+        sys.exit(0)
+    args.plot_dir += "/"
+
+    # Figure out which file has the max observation length.
+    max_obs_len = 0
+    max_ii = 0
+    args.file_list = sorted(args.file_list)
+    for ii, file in enumerate(args.file_list):
+        wf = bl.Waterfall(file, max_load=1000)
+        _, data = wf.grab_data(f_start=0, f_stop=0)
+        nints = data.shape[0]
+        this_obs_len = nints * wf.header["tsamp"]
+        if this_obs_len > max_obs_len:
+            max_obs_len = this_obs_len
+            max_ii = ii
+
+    # Swap the last file with the winner.
+    temp = args.file_list[-1]
+    args.file_list[-1] = args.file_list[max_ii]
+    args.file_list[max_ii] = temp
 
     # Make the plots.
     logger.info("Begin")
-    args.plot_dir += "/"
-    png_collection = make_waterfall_plots(args.file_list,
-                                          args.plot_dir,
-                                          args.width,
-                                          args.height,
-                                          args.dpi,
-                                          args.f_start,
-                                          args.f_stop)
-    if args.stitch != "n":
-        path_saved_png = args.plot_dir + "stax_stitched_" + args.stitch + ".png"
-        image_stitch(args.stitch, png_collection, path_saved_png)
-        for file in png_collection:
-            os.remove(file)
-
+    make_waterfall_plots(args.file_list, args.plot_dir, args.f_start, args.f_stop)
     logger.info("End")
 
 
