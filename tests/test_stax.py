53e903f6973498d279696a2983b28765d7b41bfe
Richard Elkins <richard.elkins@gmail.com>
2021-08-18 17:37:59 -0500

    Add files via upload

--- tests/test_stax.py
+++ tests/test_stax.py
@@ -10,17 +10,6 @@ def test_stax():
     cmd_tool(args)
     args = [voyager_fil, voyager_h5, "--plot_dir", dir, "--f_start", "8419", "--f_stop", "8420"]
     cmd_tool(args)
-    args = [voyager_fil, voyager_h5, "--plot_dir", dir, "--dpi", "100", "--width", "6", "--height", "9"]
-    cmd_tool(args)
-    with pytest.raises(SystemExit):
-        args = [voyager_fil, voyager_h5, "--plot_dir", dir, "--dpi", "0", "--width", "6", "--height", "9"]
-        cmd_tool(args)
-    with pytest.raises(SystemExit):
-        args = [voyager_fil, voyager_h5, "--plot_dir", dir, "--dpi", "100", "--width", "0", "--height", "9"]
-        cmd_tool(args)
-    with pytest.raises(SystemExit):
-        args = [voyager_fil, voyager_h5, "--plot_dir", dir, "--dpi", "100", "--width", "6", "--height", "0"]
-        cmd_tool(args)
 
 
 if __name__ == "__main__":
