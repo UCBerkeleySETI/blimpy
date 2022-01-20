3c952110aef7f94766fe69e6ee2cb6ff3a0be15a
Richard Elkins <richard.elkins@gmail.com>
2022-01-20 16:21:52 -0600

    Bump version to 2.0.34

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup.py -- setup script for use of packages.
 """
 from setuptools import setup, find_packages
 
-__version__ = '2.0.33'
+__version__ = '2.0.34'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
